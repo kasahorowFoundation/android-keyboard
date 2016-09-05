52380f4c69a7443940613d737aebf76f548088a6
Jojoo Imbeah <jojoo.imbeah@gmail.com>
2016-09-05 00:25:22 +0000

    Upgrade to new AnySoft version

--- buildSrc/src/main/groovy/utils/Utils.groovy
+++ buildSrc/src/main/groovy/utils/Utils.groovy
@@ -1,7 +1,29 @@
 package utils
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 class Utils {
     static def isEmpty(value) {
         return value == null || value.equals("")
     }
+
+    public static boolean isEmpty(String value) {
+      if (value == null)
+        return true;
+      else if (value.equals(""))
+        return true;
+      else
+        return false;
+    }
+
+    public static String buildVersionName(int major, int minor, boolean isSnapshot) {
+      DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+      Date date = new Date();
+      String version = ""+major+"."+minor+"."+dateFormat.format(date);
+      if (isSnapshot)
+        version += "-SNAPSHOT";
+      return version;
+    }
+
 }
