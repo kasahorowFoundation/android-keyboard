48f4cbfa5b69087b24ec8e206d3acace1fbf2b95
Henry Addo <henry@addhen.org>
2020-09-05 11:32:18 +0200

    Fix code formatting

--- ime/kasahorow/src/main/java/com/anysoftkeyboard/wordusageupload/KasahorowWordsUploader.java
+++ ime/kasahorow/src/main/java/com/anysoftkeyboard/wordusageupload/KasahorowWordsUploader.java
@@ -1,16 +1,13 @@
 package com.anysoftkeyboard.wordusageupload;
 
 import android.util.Log;
-
 import com.anysoftkeyboard.nextword.BuildConfig;
 import com.anysoftkeyboard.nextword.NextWordsContainer;
 import com.anysoftkeyboard.nextword.NextWordsStorage;
-
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -34,21 +31,22 @@ public class KasahorowWordsUploader {
         boolean areStoredNextWordsEmpty = true;
         for (NextWordsContainer container : mNextWordsStorage.loadStoredNextWords()) {
             areStoredNextWordsEmpty = false;
-            if (BuildConfig.DEBUG) Log.d(TAG, "Loaded for "+locale +" " + container);
+            if (BuildConfig.DEBUG) Log.d(TAG, "Loaded for " + locale + " " + container);
             KasahorowData kasahorowData = new KasahorowData(container.word);
             stringBuilder.append(kasahorowData).append("\n");
         }
         int code = 0;
         if (areStoredNextWordsEmpty) return code;
-        String url = "https://"+locale+PATH;
-        if (BuildConfig.DEBUG) Log.d(TAG, "upload url " + url+ "data: "+stringBuilder.toString());
-        RequestBody body = new FormBody.Builder().add(locale,stringBuilder.toString()).build();
+        String url = "https://" + locale + PATH;
+        if (BuildConfig.DEBUG)
+            Log.d(TAG, "upload url " + url + "data: " + stringBuilder.toString());
+        RequestBody body = new FormBody.Builder().add(locale, stringBuilder.toString()).build();
         Request request = requestBuilder.url(url).post(body).build();
         try (Response response = new OkHttpClient().newCall(request).execute()) {
-            if (BuildConfig.DEBUG) Log.d(TAG, "response code for " + response.code() );
+            if (BuildConfig.DEBUG) Log.d(TAG, "response code for " + response.code());
             code = response.code();
         } catch (IOException e) {
-            Log.e(TAG,"HTTP upload request failed",e);
+            Log.e(TAG, "HTTP upload request failed", e);
         }
         return code;
     }
@@ -69,7 +67,7 @@ public class KasahorowWordsUploader {
 
         @Override
         public String toString() {
-            return word+"\t"+date;
+            return word + "\t" + date;
         }
 
         private String parseDate(Date date) {
