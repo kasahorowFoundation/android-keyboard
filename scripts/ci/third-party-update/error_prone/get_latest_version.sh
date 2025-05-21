2bc22da9507add78b1ed26314f2cbbfad4a4c630
Henry Addo <henry@addhen.org>
2025-05-20 17:53:03 -0700

    Strip 'v' prefix from Error Prone version numbers

--- scripts/ci/third-party-update/error_prone/get_latest_version.sh
+++ scripts/ci/third-party-update/error_prone/get_latest_version.sh
@@ -6,4 +6,5 @@ source scripts/ci/third-party-update/version_grep_regex.sh
 LATEST_VERSION_PLUGIN=$(./scripts/ci/third-party-update/get_latest_github_version.sh "tbroyer/gradle-errorprone-plugin")
 LATEST_VERSION_TOOL=$(./scripts/ci/third-party-update/get_latest_github_version.sh "google/error-prone")
 
-echo "$LATEST_VERSION_PLUGIN $LATEST_VERSION_TOOL"
+# Modified line to strip any 'v' prefix from both variables
+echo "${LATEST_VERSION_PLUGIN#v} ${LATEST_VERSION_TOOL#v}"
